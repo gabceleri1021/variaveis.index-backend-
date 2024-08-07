b2c965cbdb9b2b447e259f349e0d4b1ddf8a21a5
gabceleri1021 <Gabriel.Pedreiro.Senai404@gmail.com>
2024-08-07 15:32:05 -0300

    Add files via upload

---
+++ variaveis.php
@@ -0,0 +1,15 @@
+<?php
+
+//declarações das variaveis constantes 
+
+define('BD_URL', 'endereco_bd_dev');
+
+define('BD_USUARIO', 'usuario_dev');
+
+define('BD_SENHA','senha_dev');
+
+echo BD_URL . '<br/>';
+echo BD_USUARIO . '<br/>';
+echo BD_SENHA . '<br/>';
+
+?>
